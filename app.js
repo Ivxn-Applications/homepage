5545aac55470754f4d7575af474c60dd4dbf26ff
Ivan Ortiz <Ivan.Ortiz@ibm.com>
2018-07-03 16:49:31 -0500

    testing 3

--- app.js
+++ app.js
@@ -1,57 +1,35 @@
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+var book = require('./routes/route');
+var app = express();
 
-// create the express app
-const app = express()
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
 
-// create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// Catch all routes and redirect to the index file
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Create default port to serve the app on
-const port = process.env.PORT || 5000
-app.listen(port)
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-// Log to feedback that this is actually running
-console.log('Server started on port ' + port)
-//
-// var book = require('./routes/route');
-// var app = express();
-//
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({'extended':'false'}));
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.use('/books', express.static(path.join(__dirname, 'dist')));
-// app.use('/book', book);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-// module.exports = app;
+module.exports = app;
